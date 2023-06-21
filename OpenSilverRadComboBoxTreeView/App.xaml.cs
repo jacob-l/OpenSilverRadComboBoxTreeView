9f5176e2b4d6bb8737230fd03adc93c4f3387251
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-06-21 16:26:05 -0700

    Add project files.

---
+++ OpenSilverRadComboBoxTreeView/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Telerik.Windows.Controls.Animation;
+
+namespace OpenSilverRadComboBoxTreeView
+{
+    public sealed partial class App : Application
+    {
+        public App()
+        {
+            AnimationManager.IsGlobalAnimationEnabled = false;
+            Host.Settings.EnableOptimizationWhereCollapsedControlsAreNotLoaded = true;
+
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+
+            var mainPage = new MainPage();
+            Window.Current.Content = mainPage;
+        }
+    }
+}
---
+++ OpenSilverRadComboBoxTreeView/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Telerik.Windows.Controls;
+using SelectionChangedEventArgs = Telerik.Windows.Controls.SelectionChangedEventArgs;
+
+namespace OpenSilverRadComboBoxTreeView
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+        }
+
+        private void TreeView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var treeView = (RadTreeView)sender;
+            var selectedItem = treeView.SelectedItem.ToString();
+
+            MyDropDownButton.Content = selectedItem;
+            MyDropDownButton.IsOpen = false; // Close the dropdown
+        }
+    }
+}
